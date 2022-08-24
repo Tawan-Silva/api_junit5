707f97e7e04b5fac92e2c0a122804e698e5e6bef
Tawan Silva <tawan.tls43@gmail.com>
2022-08-24 16:46:49 -0300

    endpoint findall para user

--- src/main/java/br/com/tawandev/api/resources/UserResource.java
+++ src/main/java/br/com/tawandev/api/resources/UserResource.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping(value = "/user")
 public class UserResource {
@@ -25,5 +28,12 @@ public class UserResource {
     public ResponseEntity<UserDTO> findBuId(@PathVariable Integer id) {
         return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDTO.class));
     }
+
+    public ResponseEntity<List<UserDTO>> findAll() {
+        return ResponseEntity.ok()
+                .body(service.findAll()
+                        .stream().map(x -> mapper.map(x, UserDTO.class)).collect(Collectors.toList()));
+    }
+
 
 }
--- src/main/java/br/com/tawandev/api/services/UserService.java
+++ src/main/java/br/com/tawandev/api/services/UserService.java
@@ -2,7 +2,11 @@ package br.com.tawandev.api.services;
 
 import br.com.tawandev.api.domain.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User findById(Integer id);
+
+    List<User> findAll();
 }
