8cffc138cd12f1f4bdcc29534ee923715a2a1e9b
Tawan Silva <tawan.tls43@gmail.com>
2022-08-24 17:16:05 -0300

    endpoint create para user

--- src/main/java/br/com/tawandev/api/services/UserService.java
+++ src/main/java/br/com/tawandev/api/services/UserService.java
@@ -1,6 +1,7 @@
 package br.com.tawandev.api.services;
 
 import br.com.tawandev.api.domain.User;
+import br.com.tawandev.api.domain.dto.UserDTO;
 
 import java.util.List;
 
@@ -9,4 +10,6 @@ public interface UserService {
     User findById(Integer id);
 
     List<User> findAll();
+
+    User create(UserDTO obj);
 }
