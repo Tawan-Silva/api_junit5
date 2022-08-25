e61e4bf6a35a37f0cde7366605a0c928551d0fb9
Tawan Silva <tawan.tls43@gmail.com>
2022-08-24 23:37:38 -0300

    teste para classe user service metodo update com excecao

--- src/test/java/br/com/tawandev/api/services/impl/UserServiceImplTest.java
+++ src/test/java/br/com/tawandev/api/services/impl/UserServiceImplTest.java
@@ -116,8 +116,20 @@ class UserServiceImplTest {
             assertEquals("E-mail já cadastrado no sistema", e.getMessage());
         }
 
-
         User response = service.create(userDTO);
+        assertNotNull(response);
+        assertEquals(User.class, response.getClass());
+        assertEquals(ID, response.getId());
+        assertEquals(NAME, response.getName());
+        assertEquals(EMAIL, response.getEmail());
+        assertEquals(PASSWORD, response.getPassword());
+    }
+
+    @Test
+    void whenUpdateThenReturnSuccess() {
+        when(repository.save(any())).thenReturn(user);
+
+        User response = service.update(userDTO);
         assertNotNull(response);
         assertEquals(User.class, response.getClass());
         assertEquals(ID, response.getId());
