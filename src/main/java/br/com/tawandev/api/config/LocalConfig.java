f925bb00cc28cb8308bc01557f913935c4fcae45
Tawan Silva <tawan.tls43@gmail.com>
2022-08-26 20:45:32 -0300

    adicionado cors na aplicacao

--- src/main/java/br/com/tawandev/api/config/LocalConfig.java
+++ src/main/java/br/com/tawandev/api/config/LocalConfig.java
@@ -3,18 +3,33 @@ package br.com.tawandev.api.config;
 import br.com.tawandev.api.domain.User;
 import br.com.tawandev.api.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
 @Profile("local")
-public class LocalConfig {
+public class LocalConfig implements WebMvcConfigurer {
+
+    @Value("${cors.originPatterns:default}")
+    private String corsOriginPatterns = "";
 
     @Autowired
     private UserRepository repository;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        var allowedOrigins = corsOriginPatterns.split(",");
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins(allowedOrigins)
+                .allowCredentials(true);
+    }
 
     @Bean
     public void startDB() {
