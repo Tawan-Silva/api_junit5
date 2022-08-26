0d02c4751e8cccc7a576aee92b87810f4b9aaf7b
Tawan Silva <tawan.tls43@gmail.com>
2022-08-26 19:48:54 -0300

    add descricao do endpoint user para documentacao swagger

--- src/main/java/br/com/tawandev/api/config/OpenApiConfig.java
+++ src/main/java/br/com/tawandev/api/config/OpenApiConfig.java
@@ -1,38 +1,36 @@
 package br.com.tawandev.api.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-
-import java.util.Collection;
-import java.util.Collections;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
 public class OpenApiConfig {
 
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("br.com.tawandev.api.resources"))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo());
     }
 
     private ApiInfo apiInfo() {
-        return new ApiInfo(
-                "REST API with Java 18 Spring Boot 2.7",
-                "v1",
-                "REST API com Testes Unitários",
-                "Terms of service",
-                new Contact("Tawan Silva", "https://github.com/Tawan-Silva", "tawan.tls43@gmail.com"),
-                "Licence of API", "API licence URL", Collections.emptyList());
+        return new ApiInfoBuilder()
+                .title("Spring Boot REST API")
+                .description("Aplicação para CRUD de Usuários com testes Unitários.")
+                .version("1.0.0")
+                .contact(new Contact("Tawan Silva", "https://github.com/Tawan-Silva", "tawan.tls43@gmail.com"))
+                .build();
     }
 }
